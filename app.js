2092fa23091bdd0c7df7dc229e4645c0e250ffe8
yeshwanth <yeshw.yeshwanth15@gmail.com>
2023-05-24 16:54:47 +0530

    added signup and signin,migrations

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
+app.get('/',(request, response) =>{
+    response.render('index');
+});
 
+app.get('/login',(request, response) => {
+    response.render('signin');
+});
+app.get('/signup',(request, response) => {
+    response.render('signup');
+});
 
 module.exports = app
---
+++ migrations/20230524111115-create-sport.js
@@ -0,0 +1,28 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('sports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      sport_name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('sports');
+  }
+};
